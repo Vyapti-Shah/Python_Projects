18293ebb72cbd9309cbdb8aeaadbf348983fc13a
Vyapti-Shah <vyaptishah94@gmail.com>
2024-08-05 20:48:37 +0530

    first commit

---
+++ 2Dlists.py
@@ -0,0 +1,15 @@
+#2D lists - a list of lists
+
+drinks = ["coffee", "tea", "milk"]
+dinner = ["khichadi", "quesadilla", "dal dhokli"]
+deserts = ["cake", "ice cream"]
+
+food = [drinks,dinner,deserts]
+
+print(food)
+print(food[0])
+print(food[0][0])
+print(food[1])
+print(food[1][2])
+print(food[2])
+print(food[2][1])
